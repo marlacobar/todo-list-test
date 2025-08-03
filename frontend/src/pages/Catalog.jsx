72e7082b3e41835d6155df47fae40040b8f18a95
Martha <43352258+marlacobar@users.noreply.github.com>
2025-08-02 18:34:40 -0600

    feat: se agrega funcionalidad para actualizar posición de automóviles & se incluye el mapa (Leaflet) y la funcionalidad para visualizar los automóviles

--- frontend/src/pages/Catalog.jsx
+++ frontend/src/pages/Catalog.jsx
@@ -1,11 +1,17 @@
 import '../styles/Catalog.css'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+
 import axios from '../services/apiInstance';
+
 
 const Catalog = () => {
   const navigate = useNavigate()
   const [cars, setCars] = useState([]);
+
   const [showModal, setShowModal] = useState(false);
   const [editingCarId, setEditingCarId] = useState(null);
   const [form, setForm] = useState({
@@ -17,6 +23,13 @@ const Catalog = () => {
     longitude: '',
   });
 
+  const [showLocationModal, setShowLocationModal] = useState(false);
+  const [selectedCar, setSelectedCar] = useState(null);
+  const [locationForm, setLocationForm] = useState({
+    latitude: '',
+    longitude: ''
+  });
+
   const fetchCars = async () => {
     try {
       const res = await axios.get('/car');
@@ -25,6 +38,21 @@ const Catalog = () => {
       console.error(err);
     }
   };
+
+  useEffect(() => {
+    const hasRefreshToken = document.cookie.includes('refreshToken');
+    if (!hasRefreshToken) {
+      navigate('/');
+    }
+    fetchCars();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    fetchCars();
+    const interval = setInterval(fetchCars, 10000);
+    return () => clearInterval(interval);
+  }, []);
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -90,47 +118,147 @@ const Catalog = () => {
     });
   };
 
-  useEffect(() => {
-    const hasRefreshToken = document.cookie.includes('refreshToken');
-    if (!hasRefreshToken) {
-      navigate('/');
-    }
+  const handleOpenLocationModal = (car) => {
+    setSelectedCar(car);
+    setLocationForm({
+      latitude: car.latitude || '',
+      longitude: car.longitude || ''
+    });
+    setShowLocationModal(true);
+  };
 
-    fetchCars();
-  }, []);
+  const handleUpdateLocation = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.patch(`/car/${selectedCar.car_id}/position`, locationForm);
+      fetchCars();
+      handleCloseLocationModal();
+
+    } catch (err) {
+      console.error('Error al actualizar posición:', err);
+    }
+  };
+
+  function MapBounds({ cars }) {
+    const map = useMap();
+
+    useEffect(() => {
+      if (!cars || cars.length === 0) return;
+
+      // Filtrar solo autos con latitud/longitud
+      const validCars = cars.filter(car => car.latitude && car.longitude);
+
+      if (validCars.length === 0) return;
+
+      // Crear un array de posiciones [latitud/longitud]
+      const positions = validCars.map(car => [parseFloat(car.latitude), parseFloat(car.longitude)]);
+
+      // Crear bounds con Leaflet
+      const bounds = L.latLngBounds(positions);
+
+      // Ajustar vista
+      map.fitBounds(bounds, { padding: [50, 50] });
+    }, [cars, map]);
+
+    return null;
+  };
+
+  const handleCloseLocationModal = () => {
+    setShowLocationModal(false);
+    setSelectedCar(null);
+    setLocationForm({
+      latitude: '',
+      longitude: ''
+    });
+  };
+
 
   return (
-    <div className="catalog-container">
+    <div className='catalog-container'>
       <h2>Catálogo de Autos</h2>
-      <button onClick={() => setShowModal(true)} className="add-btn">
+      <button onClick={() => setShowModal(true)} className='add-btn'>
         Agregar auto
       </button>
 
-      <ul className="car-list">
+      <ul className='car-list'>
         {cars.map((car) => (
-          <li key={car.car_id} className="car-item">
+          <li key={car.car_id} className='car-item'>
             <strong>{car.license_plate}</strong> - {car.brand} {car.model} ({car.color})
-            <button className="edit-btn" onClick={() => handleEdit(car)}>Editar</button>
-            <button className="delete-btn" onClick={() => handleDelete(car.car_id)}>Eliminar</button>
+            <button className='edit-btn' onClick={() => handleEdit(car)}>Editar</button>
+            <button className='delete-btn' onClick={() => handleDelete(car.car_id)}>Eliminar</button>
+            <button className='location-btn' onClick={() => handleOpenLocationModal(car)}>Actualizar posición</button>
           </li>
         ))}
       </ul>
 
-      {showModal && (
-        <div className="modal-overlay">
-          <div className="modal">
-            <h3>{editingCarId ? 'Editar automóvil' : 'Agregar automóvil'}</h3>
-            <form onSubmit={handleSubmit} className="form">
-              <input name="license_plate" value={form.license_plate} onChange={handleChange} placeholder="Placas" required />
-              <input name="brand" value={form.brand} onChange={handleChange} placeholder="Marca" />
-              <input name="model" value={form.model} onChange={handleChange} placeholder="Modelo" />
-              <input name="color" value={form.color} onChange={handleChange} placeholder="Color" />
-              <input name="latitude" value={form.latitude} onChange={handleChange} placeholder="Latitud" />
-              <input name="longitude" value={form.longitude} onChange={handleChange} placeholder="Longitud" />
+      {/* Mapa de autos */}
+      <MapContainer id='map' center={[20.6736, -103.344]} zoom={8} >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cars.map((car) =>
+          car.latitude && car.longitude ? (
+            <Marker key={car.car_id} position={[car.latitude, car.longitude]}>
+              <Popup>
+                <strong>{car.license_plate}</strong><br />
+                {car.brand} {car.model}
+              </Popup>
+            </Marker>
+          ) : null
+        )}
+        <MapBounds cars={cars} />
+      </MapContainer>
 
-              <div className="form-buttons">
-                <button type="submit">Guardar</button>
-                <button type="button" onClick={handleCloseModal}>Cancelar</button>
+
+      {/* Modal para agregar/editar automóvil */}
+      {showModal && (
+        <div className='modal-overlay'>
+          <div className='modal'>
+            <h3>{editingCarId ? 'Editar automóvil' : 'Agregar automóvil'}</h3>
+            <form onSubmit={handleSubmit} className='form'>
+              <input name='license_plate' value={form.license_plate} onChange={handleChange} placeholder='Placas' required />
+              <input name='brand' value={form.brand} onChange={handleChange} placeholder='Marca' />
+              <input name='model' value={form.model} onChange={handleChange} placeholder='Modelo' />
+              <input name='color' value={form.color} onChange={handleChange} placeholder='Color' />
+              <input name='latitude' value={form.latitude} onChange={handleChange} placeholder='Latitud' />
+              <input name='longitude' value={form.longitude} onChange={handleChange} placeholder='Longitud' />
+
+              <div className='form-buttons'>
+                <button type='submit'>Guardar</button>
+                <button type='button' onClick={handleCloseModal}>Cancelar</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+
+
+      {/* Modal para editar la posición de un automóvil */}
+      {showLocationModal && (
+        <div className='modal-overlay'>
+          <div className='modal'>
+            <h3>Actualizar posición</h3>
+            <form onSubmit={handleUpdateLocation} className='form'>
+              <input
+                name='latitude'
+                value={locationForm.latitude}
+                onChange={(e) => setLocationForm({ ...locationForm, latitude: e.target.value })}
+                placeholder='Latitud'
+                required
+              />
+              <input
+                name='longitude'
+                value={locationForm.longitude}
+                onChange={(e) => setLocationForm({ ...locationForm, longitude: e.target.value })}
+                placeholder='Longitud'
+                required
+              />
+
+              <div className='form-buttons'>
+                <button type='submit'>Actualizar</button>
+                <button type='button' onClick={handleCloseLocationModal}>Cancelar</button>
               </div>
             </form>
           </div>
